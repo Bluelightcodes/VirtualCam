1982419c2af9f12067da06ea301a22661e866831
Bluelight <127167266+Bluelightcodes@users.noreply.github.com>
2025-06-21 17:18:25 +0530

    Added plane added distance and rotation feature

--- App.tsx
+++ App.tsx
@@ -4,80 +4,123 @@ import {
   ViroText,
   ViroTrackingReason,
   ViroTrackingStateConstants,
-  ViroMaterials
+  ViroMaterials,
+  ViroQuad,
+  ViroSphere
 } from "@reactvision/react-viro";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import { TouchableOpacity, Text,View,Button,StyleSheet } from "react-native";
 
 ViroMaterials.createMaterials({
   planeMaterial: {
     diffuseColor: 'rgba(0,0,255,0.5)', // Semi-transparent blue for detected planes
   },
-  selectedPlaneMaterial: {
-    diffuseColor: 'rgba(0,255,0,0.7)', // Semi-transparent green for selected plane
-  },
-  placedObjectMaterial: {
-    diffuseColor: '#FF0000', // Red for the placed object
-  },
+  reddot:{
+    diffuseColor:'rgb(255, 0, 0)'
+  }
 });
 
+//Contexts
 const TextContext = React.createContext("");
+const StatusContext = React.createContext({
+  statusState: { status: "", textcolor: "" },
+  setStatusState: (value: { status: string; textcolor: string }) => {},
+});
+const CamContext = React.createContext({
+  camstate:{cameraPosition:[0,0,0],
+            cameraRotation:[0,0,0]},
+  setcamstate: (_: any) => {},
+});
+
+const GridContext = React.createContext
+
+//Contexts
+
 
 const HelloWorldSceneAR = () => {
-  const [text, setText] = useState("Initializing AR...");
-  const message = React.useContext(TextContext);
+  //using contexts
 
-  const [planes, detectedplaneadder]=useState({});//identifies different planes
-  const [selectedplaneid, setplane]=useState(null);//for selecting plane
-  const [objectposition, setdobjectposition]=useState(null); //for setting obect position
+  const { statusState, setStatusState } = useContext(StatusContext);//stores tracked
+  const { setcamstate } = useContext(CamContext);
+  const camupdate = ({position,rotation,}: {position: [number, number, number];rotation: [number, number, number];}) => {
+  if (statusState.status !== "Tracked") return;
+  const relposition:[number,number,number]=[position[0]-0,position[1]+0.5,position[2]+1]
+  setcamstate({
+    cameraPosition: relposition,
+    cameraRotation: rotation,
+  });
+  // console.log(relposition)
+  // console.log(rotation)
+};
+//states
 
   //--------------------------------------DURING initialisation--------------------------------------------------------
   function onInitialized(state: any, reason: ViroTrackingReason) 
   {
     console.log("onInitialized", state, reason);
-    if (state === ViroTrackingStateConstants.TRACKING_NORMAL) {
-      //setText(props.message);
+    if (statusState.status !== "Tracked" && state === ViroTrackingStateConstants.TRACKING_NORMAL) {
+      setTimeout(()=>{setStatusState({ status: "Tracked", textcolor: "green" });},100)
     } else if (state === ViroTrackingStateConstants.TRACKING_UNAVAILABLE) {
-      //setText("Tracking lost");
+      setTimeout(()=>{setStatusState({ status: "Tracking lost", textcolor: "red" });
+                      setcamstate({cameraPosition: [0,0,0],cameraRotation: [0,0,0],});},100)
     }
   }
   //--------------------------------------AFTER initialisation--------------------------------------------------------
-  useEffect(() => {setText(message);}, [message]);
 
   return (
     
-    <ViroARScene onTrackingUpdated={onInitialized}>
-      <ViroText
-        text={text}
-        scale={[0.5, 0.5, 0.5]}
-        position={[0, 0, -1]}
-        style={styles.helloWorldTextStyle}
-      />
+    <ViroARScene onTrackingUpdated={onInitialized} onCameraTransformUpdate={camupdate}>
+      {statusState.status == "Tracked" && (
+    <View>
+    <ViroQuad
+      position={[0, -0.5, -1]}
+      rotation={[-90, 0, 0]}
+      width={1}
+      height={1}
+      materials={["planeMaterial"]}
+    />
+      <ViroSphere
+      radius={0.01}
+      position={[0, -0.5, -1]}  // same as quad center
+      materials={['reddot']}
+    />
+    </View>    
+    )}
     </ViroARScene>
   );
 };
   //----------------------------------------------------------------------------------------------
 export default () => {
-  const [textbox,settextbox]=useState('Placeholder');
-  return (
-    <TextContext.Provider value={textbox}>
-    <View style={styles.container}>
+  const [camstate,setcamstate]=useState({cameraPosition:[0,0,0],cameraRotation:[0,0,0]});
+  const [statusState, setStatusState] = useState({
+  status: 'Initializing AR..',
+  textcolor: 'rgb(94, 110, 0)'
+}); //initilises status and status color while initilising
 
-    <ViroARSceneNavigator
-      autofocus={true}
-      initialScene={{
-        scene: HelloWorldSceneAR,
-      }}
-      style={styles.f1}
-    />
-    <View style={styles.overlay}>
-      <Text style={{bottom:50}}>{textbox}</Text>
-          <TouchableOpacity style={styles.button} onPress={() => settextbox("Button Was Clicked!")}>
-      <Text style={styles.buttonText}>{'Start'}</Text>
-    </TouchableOpacity>
-      </View>
+  return (
+      <StatusContext.Provider value={{ statusState, setStatusState }}>
+        <CamContext.Provider value={{camstate,setcamstate}}>
+        <View style={styles.container}>
+          <ViroARSceneNavigator
+          autofocus={true}
+          initialScene={{
+            scene: HelloWorldSceneAR,
+          }}
+          style={styles.f1}
+          />  
+        <View style={styles.overlay}>
+          <View style={styles.statusBox}><Text style={[styles.statusText, { color: 'black'}]}>{camstate.cameraPosition.join(", ")}</Text></View>
+          <View style={styles.statusBox}><Text style={[styles.statusText, { color: 'black'}]}>{camstate.cameraRotation.join(", ")}</Text></View>
+          <View style={styles.statusBox}><Text style={[styles.statusText, { color: statusState.textcolor }]}>{statusState.status}</Text></View>
+              <TouchableOpacity
+              style={styles.button}
+              onPress={() => {}}>
+              <Text style={styles.buttonText}>Start</Text>
+              </TouchableOpacity>
+          </View>
     </View>
-    </TextContext.Provider>
+    </CamContext.Provider>
+    </StatusContext.Provider>
   );
 };
 
@@ -114,5 +157,18 @@ var styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: "bold",
   },
+  statusBox: {
+  backgroundColor: 'rgb(255, 255, 255)', // translucent white
+  paddingHorizontal: 16,
+  paddingVertical: 8,
+  borderRadius: 12,
+  marginBottom: 10,
+},
+
+statusText: {
+  fontSize: 16,
+  fontWeight: 'bold',
+  textAlign: 'center',
+},
 
 });
