e43435f884ff7307e30bcfa882ef49e2b6f326f2
Bluelight <127167266+Bluelightcodes@users.noreply.github.com>
2025-06-21 06:34:19 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -6,9 +6,10 @@ import {
   ViroTrackingStateConstants,
 } from "@reactvision/react-viro";
 import React, { useState } from "react";
-import { StyleSheet } from "react-native";
+import { TouchableOpacity, Text,View,Button,StyleSheet } from "react-native";
 
 const HelloWorldSceneAR = () => {
+
   const [text, setText] = useState("Initializing AR...");
 
   function onInitialized(state: any, reason: ViroTrackingReason) {
@@ -16,7 +17,7 @@ const HelloWorldSceneAR = () => {
     if (state === ViroTrackingStateConstants.TRACKING_NORMAL) {
       setText("Hello World!");
     } else if (state === ViroTrackingStateConstants.TRACKING_UNAVAILABLE) {
-      // Handle loss of tracking
+      setText("Nigga?");
     }
   }
 
@@ -33,7 +34,10 @@ const HelloWorldSceneAR = () => {
 };
 
 export default () => {
+  const [textbox,settextbox]=useState('Placefolder');
   return (
+    <View style={styles.container}>
+
     <ViroARSceneNavigator
       autofocus={true}
       initialScene={{
@@ -41,6 +45,13 @@ export default () => {
       }}
       style={styles.f1}
     />
+    <View style={styles.overlay}>
+      <Text style={{bottom:50}}>{textbox}</Text>
+          <TouchableOpacity style={styles.button} onPress={() => settextbox("Button Was Clicked!")}>
+      <Text style={styles.buttonText}>{'RESET'}</Text>
+    </TouchableOpacity>
+      </View>
+    </View>
   );
 };
 
@@ -53,4 +64,29 @@ var styles = StyleSheet.create({
     textAlignVertical: "center",
     textAlign: "center",
   },
+  container:{
+    flex: 1,
+  },
+  overlay: {
+    position: "absolute",
+    bottom: 50,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+    button: {
+    backgroundColor: "white",
+    width: 70,
+    height: 70,
+    borderRadius: 35, // Half of width/height
+    justifyContent: "center",
+    alignItems: "center",
+    elevation: 5, // For Android shadow
+  },
+  buttonText: {
+    color: "black",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+
 });
